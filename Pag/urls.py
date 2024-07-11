ba99e14ffcba7d9ca05c3e490b42ec8f4aef6a9e
ElCreduloMcFly <gas.sanchez@duocuc.cl>
2024-07-11 17:21:57 -0400

    funciones listas
    todo lo funcional LISTO

--- Pag/urls.py
+++ Pag/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import mostrarhome,mostrarcarrito,mostrarherramientas,mostrarinicio,mostrarvendedor,iniciarsesion,registrar,finsesion,mostraragregar,agregarproducto,eliminarproducto,modificarproducto,mostrarproducto,mostrarnuevotrabajador,mostrarmenulogin,mostrarproveedor,mostrarproveedores,agregarequipo,agregarProveedor,mostrarequipos,eliminarproveedor,agregartrabajador,agregarequipos
+from .views import mostrarhome,mostrarcarrito,mostrarherramientas,mostrarinicio,mostrarvendedor,iniciarsesion,registrar,finsesion,mostraragregar,agregarproducto,eliminarproducto,modificarproducto,mostrarproducto,mostrarnuevotrabajador,mostrarmenulogin,mostrarproveedor,mostrarproveedores,agregarequipo,agregarProveedor,mostrarequipos,eliminarproveedor,agregartrabajador,agregarequipos,eliminarequipo
 
 urlpatterns = [
     path('',mostrarhome,name="MenuPrincipal"),
@@ -23,6 +23,7 @@ urlpatterns = [
     path('agregarEquipos/',agregarequipos, name='agregarEquipos'),
     path('agregarprove/',agregarProveedor, name='agregarprove'),
     path('eliminarproducto/<id_prod>',eliminarproducto, name='eliminarproducto'),
+    path('eliminarequipo/<id_equipo>',eliminarequipo, name='eliminarequipo'),
     path('eliminarproveedor/<id_prov>',eliminarproveedor, name='eliminarproveedor'),
     path('mostrarproducto/<int:id_prod>',mostrarproducto, name='mostrarproducto'),
     path('modificarproducto/<int:id_prod>',modificarproducto, name='modificarproducto'),
